63de247409903fc16a4e52fbe322754649c0e200
Мальцев Николай <Malzewn@gmail.com>
2023-05-05 19:29:49 +0300

    Добавьте файлы проекта.

---
+++ Lab_18/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+
+namespace Lab_18
+{
+
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void Calc_Click(object sender, RoutedEventArgs e)
+        {
+            if (Radio1.IsChecked.GetValueOrDefault())
+            {
+                double a = Convert.ToDouble(R1TextA.Text);
+                double f = Convert.ToDouble(R1ComboF.Text);
+                this.Title = "Ответ: " + Math.Sign(f * a).ToString("F");
+            }
+
+            if (Radio2.IsChecked.GetValueOrDefault())
+            {
+                double a = Convert.ToDouble(R2TextA.Text);
+                double b = Convert.ToDouble(R2TextB.Text);
+                double f = Convert.ToDouble(R2ComboF.Text);
+                this.Title = "Ответ: " + (Math.Cos(f * a)).ToString("F");
+            }
+
+            if (Radio3.IsChecked.GetValueOrDefault())
+            {
+                double a = Convert.ToDouble(R3TextA.Text);
+                double b = Convert.ToDouble(R3TextB.Text);
+                double c = Convert.ToDouble(R3ComboC.Text);
+                double d = Convert.ToDouble(R3ComboD.Text);
+                this.Title = "Ответ: " + (c * a * a + d * b * b).ToString("F");
+            }
+
+            if (Radio4.IsChecked.GetValueOrDefault())
+            {
+                double a = Convert.ToDouble(R4TextA.Text);
+                double d = Convert.ToDouble(R4TextD.Text);
+                double c = Convert.ToDouble(R4ComboC.Text);
+                double res = 1;
+                for (int i = 0; i < d; i++)
+                {
+                    res = res * (c + a) + 1;
+                }
+                this.Title = "Ответ: " + res.ToString("F");
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lab_18_ind/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab_18_ind
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void Calc_Click(object sender, RoutedEventArgs e)
+        {
+            double p = Convert.ToDouble(TextP.Text);
+            double f = Convert.ToDouble(TextF.Text);
+            double x = Convert.ToDouble(TextX.Text);
+            double y = Convert.ToDouble(TextY.Text);
+            double n = Convert.ToDouble(ComboN.Text);
+            double k = Convert.ToDouble(ComboK.Text); 
+
+            double rez = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= k; j++)
+                {
+                    rez = (Math.Pow(p, i) * Math.Pow(x, 3) * Math.Pow(f, j) * Math.Pow(y, 2)) / (i * j);
+                }
+            }
+            Label.Content = "Ответ: " + rez.ToString("F");
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
